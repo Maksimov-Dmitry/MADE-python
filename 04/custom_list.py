c2534de6273da3d1f98806bb6e20b4b865ded2d8
Maksimov-Dmitry <61335724+Maksimov-Dmitry@users.noreply.github.com>
2022-09-27 10:36:54 +0200

    Init

---
+++ 04/custom_list.py
@@ -0,0 +1,60 @@
+"""
+This module contains the CustomList class
+"""
+
+from itertools import zip_longest
+
+
+class CustomList(list):
+    """
+    This class inherits the properties and methods from list.
+    The following operators have been redefined:
+        1. CustomList +/- CustomList
+        2. CustomList +/- list
+        3. list +/- CustomList
+        4. CustomList compare CustomList (==, !=, <, <=, >, >=)
+        5. str(CustomList)
+    """
+
+    def __init__(self, lst):
+        super().__init__()
+        self.lst = lst
+
+    def __iter__(self):
+        return iter(self.lst)
+
+    def __add__(self, other):
+        new_lst = [x + y for x, y in zip_longest(self.lst, other, fillvalue=0)]
+        return CustomList(new_lst)
+
+    def __sub__(self, other, is_radd=False):
+        new_lst = [y - x if is_radd else x - y
+                   for x, y in zip_longest(self.lst, other, fillvalue=0)]
+        return CustomList(new_lst)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __rsub__(self, other):
+        return self.__sub__(other, is_radd=True)
+
+    def __lt__(self, other):
+        return sum(self.lst) < sum(other)
+
+    def __le__(self, other):
+        return sum(self.lst) <= sum(other)
+
+    def __eq__(self, other):
+        return sum(self.lst) == sum(other)
+
+    def __ne__(self, other):
+        return sum(self.lst) != sum(other)
+
+    def __gt__(self, other):
+        return sum(self.lst) > sum(other)
+
+    def __ge__(self, other):
+        return sum(self.lst) >= sum(other)
+
+    def __str__(self):
+        return f'{self.lst}, {sum(self.lst)}'
