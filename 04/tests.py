ed38ced592c4ea97d06b64ae976dd6048cddee56
Maksimov-Dmitry <61335724+Maksimov-Dmitry@users.noreply.github.com>
2022-09-27 15:00:53 +0200

    bug fixed

--- 04/tests.py
+++ 04/tests.py
@@ -9,22 +9,22 @@ CustomList_A = CustomList([5, 1, 3, 7])
 CustomList_B = CustomList([1, 2, 7])
 
 assert (CustomList_A + CustomList_B).lst == [6, 3, 10, 7]
-assert isinstance(CustomList_A + CustomList_B) == CustomList
+assert isinstance(CustomList_A + CustomList_B, CustomList)
 
 assert (CustomList_A - CustomList_B).lst == [4, -1, -4, 7]
-assert isinstance(CustomList_A - CustomList_B) == CustomList
+assert isinstance(CustomList_A - CustomList_B, CustomList)
 
 assert ([2, 3] + CustomList_B).lst == [3, 5, 7]
-assert isinstance([2, 3] + CustomList_B) == CustomList
+assert isinstance([2, 3] + CustomList_B, CustomList)
 
 assert (CustomList_B + [2, 3]).lst == [3, 5, 7]
-assert isinstance(CustomList_B + [2, 3]) == CustomList
+assert isinstance(CustomList_B + [2, 3], CustomList)
 
 assert ([2, 3] - CustomList_B).lst == [1, 1, -7]
-assert isinstance([2, 3] - CustomList_B) == CustomList
+assert isinstance([2, 3] - CustomList_B, CustomList)
 
 assert (CustomList_B - [2, 3]).lst == [-1, -1, 7]
-assert isinstance(CustomList_B - [2, 3]) == CustomList
+assert isinstance(CustomList_B - [2, 3], CustomList)
 
 assert CustomList_B < CustomList_A
 assert CustomList_B <= CustomList_A
