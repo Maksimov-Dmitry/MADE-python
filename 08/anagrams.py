e4371149527440457e8560539038fc55ba9c8e72
Maksimov-Dmitry <61335724+Maksimov-Dmitry@users.noreply.github.com>
2022-10-17 10:07:29 +0200

    find_anagrams function fixed

--- 08/anagrams.py
+++ 08/anagrams.py
@@ -4,7 +4,7 @@ from collections import Counter
 def find_anagrams(text, pattern):
     """
     Find positions of anagrams pattern in text, case sensitive.
-    Complexity: O(n), n - number of words in text
+    Complexity: O(n)
 
     Args:
         text (str): string to search for anagrams
@@ -13,11 +13,19 @@ def find_anagrams(text, pattern):
     Returns:
         anagrams_positions (list): list of anagrams positions
     """
+    if len(pattern) > len(text):
+        return []
 
+    pattern_last_letter_pos = len(pattern) - 1
     counter_pattern = Counter(pattern)
+    counter_string = Counter(text[:pattern_last_letter_pos])
     anagrams_positions = []
-    for i, word in enumerate(text.split()):
-        if counter_pattern == Counter(word):
+    for i, letter in enumerate(text[pattern_last_letter_pos:]):
+        counter_string[letter] += 1
+        if counter_string == counter_pattern:
             anagrams_positions.append(i)
+        counter_string[text[i]] -= 1
+        if counter_string[text[i]] == 0:
+            del counter_string[text[i]]
 
     return anagrams_positions
--- 08/test_anagrams.py
+++ 08/test_anagrams.py
@@ -19,8 +19,17 @@ class TestAnagrams(unittest.TestCase):
         self.assertEqual(actual, [])
 
     def test_find_anagrams(self):
-        actual = find_anagrams('car tree c boy Rac  arc arca', 'car')
-        self.assertEqual(actual, [0, 5])
+        actual = find_anagrams('cartreecboyRacarcarca', 'car')
+        self.assertEqual(actual, [0, 13, 14, 15, 16, 17, 18])
 
-        actual = find_anagrams('cAr tree boy Rac arC arca', 'car')
+        actual = find_anagrams('cArtreeboyRacarCarca', 'car')
+        self.assertEqual(actual, [12, 16, 17])
+
+        actual = find_anagrams('ca', 'car')
         self.assertEqual(actual, [])
+
+        actual = find_anagrams('CaRcAr', 'cAr')
+        self.assertEqual(actual, [3])
+
+        actual = find_anagrams('qwqw', 'qw')
+        self.assertEqual(actual, [0, 1, 2])
